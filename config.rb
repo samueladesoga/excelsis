3d5f6f32b00a60729b25c1dd6830ceb19afecbed
Osazeme I. Usen <osazemeu@yahoo.com>
2016-07-04 10:58:31 +0100

    added sitemap and started about page

--- config.rb
+++ config.rb
@@ -86,9 +86,9 @@ set :trailing_slash, false
 # activate :automatic_image_sizes
 
 # Reload the browser automatically whenever files change
-# configure :development do
-#   activate :livereload
-# end
+configure :development do
+  activate :livereload
+end
 
 # Methods defined in the helpers block are available in templates
 # helpers do
@@ -102,6 +102,8 @@ set :css_dir, 'stylesheets'
 set :js_dir, 'javascripts'
 
 set :images_dir, 'images'
+
+page "/sitemap.xml", :layout => false
 
 # Build-specific configuration
 configure :build do
--- source/javascripts/custom.js
+++ source/javascripts/custom.js
@@ -4,3 +4,23 @@ $(window).load(function() {
 directionNav: false
     });
   });
+
+ /* ---------------------------------------------------------------------- */
+ /*	Contact Map
+ /* ---------------------------------------------------------------------- */
+	var contact = {"lat": "40.714623", "lon": "-74.006605"}; //Change a map coordinate here!
+
+	try {
+	    $('#map').gmap3({
+	        action: 'addMarker',
+	        latLng: [contact.lat, contact.lon],
+	        map: {
+	            center: [contact.lat, contact.lon],
+	            zoom: 14
+	        }
+	    },
+	    {action: 'setOptions', args: [{scrollwheel: false}]}
+	    );
+	} catch (err) {
+
+	}
