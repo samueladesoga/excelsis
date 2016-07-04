ae64a9568beb40325f47d254804c67f89e68a470
Osazeme I. Usen <osazemeu@yahoo.com>
2016-07-04 22:29:06 +0100

    removed faulty sitemap

--- config.rb
+++ config.rb
@@ -103,8 +103,6 @@ set :js_dir, 'javascripts'
 
 set :images_dir, 'images'
 
-page "/sitemap.xml", :layout => false
-
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
