005d0e01b9973fd14df282793c79d040d67652f7
Osazeme I. Usen <osazemeu@yahoo.com>
2016-07-05 09:35:01 +0100

    added skill partial to contetful

--- config.rb
+++ config.rb
@@ -75,7 +75,7 @@ activate :contentful do |f|
   f.content_types = {partner: ENV['CONTENTFUL_PARTNERS'],
                       affiliated_company: ENV['CONTENTFUL_AFFILIATED_COMPANIES'],
                       misc: ENV['CONTENTFUL_MISC'], service: ENV['CONTENTFUL_SERVICES'],
-                      testimonial: ENV['CONTENTFUL_TESTIMONIAL'] }
+                      testimonial: ENV['CONTENTFUL_TESTIMONIAL'], skill: ENV['CONTENTFUL_SKILL'] }
 end
 
 # Pretty URLs (https://middlemanapp.com/advanced/pretty_urls/)
