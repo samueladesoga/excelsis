c60731df3cf5c529bfc10098ff4ca030e17b2645
Osazeme I. Usen <osazemeu@yahoo.com>
2016-07-06 02:37:40 +0100

    started changing service to list

--- source/javascripts/custom.js
+++ source/javascripts/custom.js
@@ -5,6 +5,15 @@ directionNav: false
     });
   });
 
+//jquery counter plugin
+$(document).ready(function() {
+
+$('.counter').counterUp({
+delay: 100,
+time: 5000
+});
+
+});
  /* ---------------------------------------------------------------------- */
  /*	Contact Map
  /* ---------------------------------------------------------------------- */
@@ -25,7 +34,85 @@ directionNav: false
 
 	}
 
-	$('.counter').counterUp({
-    delay: 100,
-    time: 5000
+	//isotope filter
+
+var winDow = $(window);
+// Needed variables
+var $container = $('.iso-call');
+var $filter = $('.filter');
+
+try {
+    $container.imagesLoaded(function() {
+        $container.trigger('resize');
+        $container.isotope({
+            filter: '*',
+            layoutMode: 'masonry',
+            animationOptions: {
+                duration: 750,
+                easing: 'linear'
+            }
+        });
+
+        $('.triggerAnimation').waypoint(function() {
+            var animation = $(this).attr('data-animate');
+            $(this).css('opacity', '');
+            $(this).addClass("animated " + animation);
+
+        },
+                {
+                    offset: '75%',
+                    triggerOnce: true
+                }
+        );
+        setTimeout( 1500);
+    });
+} catch (err) {
+}
+
+winDow.bind('resize', function() {
+    var selector = $filter.find('a.active').attr('data-filter');
+
+    try {
+        $container.isotope({
+            filter: selector,
+            animationOptions: {
+                duration: 750,
+                easing: 'linear',
+                queue: false
+            }
+        });
+    } catch (err) {
+    }
+    return false;
 });
+
+// Isotope Filter 
+$filter.find('a').click(function() {
+    var selector = $(this).attr('data-filter');
+
+    try {
+        $container.isotope({
+            filter: selector,
+            animationOptions: {
+                duration: 750,
+                easing: 'linear',
+                queue: false
+            }
+        });
+    } catch (err) {
+
+    }
+    return false;
+});
+
+
+var filterItemA = $('.filter li a');
+
+filterItemA.on('click', function() {
+    var $this = $(this);
+    if (!$this.hasClass('active')) {
+        filterItemA.removeClass('active');
+        $this.addClass('active');
+    }
+});
+
